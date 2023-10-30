680b218b461515d2c3688bfc3d79c909c50f3b0a
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-10-30 23:47:52 +0100

    added basic components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { PhonebookInput } from './PhonebookInput/PhonebookInput';
+import { Section } from './Section/Section';
+
 export const App = () => {
+  // state = {
+  //   contacts: [],
+  //   name: '',
+  // };
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Section title="Phonebook">
+        <PhonebookInput />
+      </Section>
+      <Section title="Contacts"></Section>
     </div>
   );
 };
---
+++ src/components/PhonebookInput/PhonebookInput.jsx
@@ -0,0 +1,13 @@
+import css from './PhonebookInput.module.css';
+
+export const PhonebookInput = ({}) => (
+  <div className={css.input}>
+    <input
+      type="text"
+      name="name"
+      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+      required
+    />
+  </div>
+);
